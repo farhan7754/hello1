ee49e087d6c31f2453723f017d972b7b0b94905f
farhan7754 <farhanuzzaman7754@gmail.com>
2024-03-13 15:57:10 +0600

    This is my first commit

---
+++ src/controllers/studentController.js
@@ -0,0 +1,12 @@
+exports.createStudent=(req,res)=>{
+    res.end("This is create function");
+}
+exports.readStudent=(req,res)=>{
+    res.end("This is read function");
+}
+exports.updateStudent=(req,res)=>{
+    res.end("This is update function");
+}
+exports.deleteStudent=(req,res)=>{
+    res.end("This is delete function");
+}
